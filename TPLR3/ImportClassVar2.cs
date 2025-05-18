85d142dcf81814c8b2341528cdcd57837fd06fd6
Григорьев Михаил <eragon09040@gmail.com>
2025-05-18 16:49:23 +0300

    Созданы классы для расчета проноза и нахождения максимальных и минимальных значений. Исправлен класс импорта.

--- TPLR3/ImportClassVar2.cs
+++ TPLR3/ImportClassVar2.cs
@@ -1,53 +1,54 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using ClosedXML.Excel;
 
-namespace TP_LR3
+public class ImportClassVar2
 {
-    public static class ImportClassVar2
+    public void ImportThreeFloatColumnsByName(
+        string filePath,
+        string columnName1, string columnName2, string columnName3, out List<DateTime> list1, out List<float> list2, out List<float> list3)
     {
-        public static void ImportThreeFloatColumnsByName(string filePath, string columnName1, string columnName2, string columnName3, out List<DateTime> list1, out List<float> list2, out List<float> list3)
+        list1 = new List<DateTime>();
+        list2 = new List<float>();
+        list3 = new List<float>();
+
+        using (var workbook = new XLWorkbook(filePath))
         {
-            list1 = new List<DateTime>();
-            list2 = new List<float>();
-            list3 = new List<float>();
+            var worksheet = workbook.Worksheet("SheetData");
+            int lastRow = worksheet.LastRowUsed().RowNumber();
 
-            using (var workbook = new XLWorkbook(filePath))
+            // Поиск индексов столбцов по названиям в первой строке
+            int colIndex1 = 0, colIndex2 = 0, colIndex3 = 0;
+            foreach (var cell in worksheet.Row(1).CellsUsed())
             {
-                var worksheet = workbook.Worksheet("SheetData");
-                int lastRow = worksheet.LastRowUsed().RowNumber();  
+                if (cell.GetString() == columnName1) colIndex1 = cell.Address.ColumnNumber;
+                if (cell.GetString() == columnName2) colIndex2 = cell.Address.ColumnNumber;
+                if (cell.GetString() == columnName3) colIndex3 = cell.Address.ColumnNumber;
+            }
 
-                // Поиск индексов столбцов по названиям в первой строке
-                int colIndex1 = 0, colIndex2 = 0, colIndex3 = 0;
-                foreach (var cell in worksheet.Row(1).CellsUsed())
-                {
-                    if (cell.GetString() == columnName1) colIndex1 = cell.Address.ColumnNumber;
-                    if (cell.GetString() == columnName2) colIndex2 = cell.Address.ColumnNumber;
-                    if (cell.GetString() == columnName3) colIndex3 = cell.Address.ColumnNumber;
-                }
+            if (colIndex1 == 0 || colIndex2 == 0 || colIndex3 == 0)
+                throw new System.Exception("Один или несколько столбцов не найдены!");
 
-                if (colIndex1 == 0 || colIndex2 == 0 || colIndex3 == 0)
-                    throw new System.Exception("Один или несколько столбцов не найдены!");
+            // Импорт значений из найденных столбцов
+            for (int row = 2; row <= lastRow; row++)
+            {
+                var dateStr = worksheet.Cell(row, colIndex1).GetString().Trim();
+                var usdStr = worksheet.Cell(row, colIndex2).GetString().Trim();
+                var eurStr = worksheet.Cell(row, colIndex3).GetString().Trim();
 
-                // Импорт значений из найденных столбцов
-                for (int row = 2; row <= lastRow; row++)
-                {
-                    if (DateTime.TryParse(worksheet.Cell(row, colIndex1).GetString().Replace(',', '.'), out DateTime val1))
-                        list1.Add(val1);
+                // Даты
+                if (DateTime.TryParse(dateStr, out DateTime val1))
+                    list1.Add(val1);
 
-                    if (float.TryParse(worksheet.Cell(row, colIndex2).GetString().Replace(',', '.'), out float val2))
-                        list2.Add(val2);
+                // Курс USD
+                if (float.TryParse(usdStr, NumberStyles.Any, CultureInfo.GetCultureInfo("ru-RU"), out float val2))
+                    list2.Add(val2);
 
-                    if (float.TryParse(worksheet.Cell(row, colIndex3).GetString().Replace(',', '.'), out float val3))
-                        list3.Add(val3);
-                }
+                // Курс EUR
+                if (float.TryParse(eurStr, NumberStyles.Any, CultureInfo.GetCultureInfo("ru-RU"), out float val3))
+                    list3.Add(val3);
             }
         }
     }
-
 }
